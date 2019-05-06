309b094873f75beacda946b979cdb9fdacc2b1f8
shubhamknoldus <shubham.verma@knoldus.in>
2019-05-06 18:20:46 +0530

    Added spring template

---
+++ src/main/java/com/knolbucks/knolbucks/KnolBucksApplication.java
@@ -0,0 +1,13 @@
+package com.knolbucks.knolbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnolBucksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnolBucksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knolbucks/knolbucks/KnolBucksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.knolbucks.knolbucks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KnolBucksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
