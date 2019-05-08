d71f124f3d353c80d7a38c0e75db8ae5f561f981
shubhamknoldus <shubham.verma@knoldus.in>
2019-05-08 17:30:03 +0530

    Added webflux related dependencies and files

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -8,5 +8,8 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        GreetingWebClient gwc = new GreetingWebClient();
+        System.out.println(gwc.getResult());
     }
 }
