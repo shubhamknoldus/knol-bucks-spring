504b93a6f9299b2de40361c50c2da75f6eb86660
shubhamknoldus <shubham.verma@knoldus.in>
2019-05-10 15:16:28 +0530

    Updated DB scripts

--- database-script/coupon-service.sql
+++ database-script/coupon-service.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
 --
--- Host: localhost    Database: knolbucks
+-- Host: localhost    Database: couponService
 -- ------------------------------------------------------
 -- Server version	5.7.26-0ubuntu0.18.04.1
 
@@ -16,35 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `knolbucks`
+-- Current Database: `couponService`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `knolbucks` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `couponService` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `knolbucks`;
+USE `couponService`;
 
+
+SET sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 --
 -- Table structure for table `couponRequests`
 --
-
-SET sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 
 DROP TABLE IF EXISTS `couponRequests`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `couponRequests` (
-  `requestId` varchar(32) NOT NULL,
-  `requesterId` varchar(10) NOT NULL,
-  `issuerId` varchar(10) NOT NULL,
+  `id` varchar(36) NOT NULL,
+  `requesterId` varchar(36) NOT NULL,
+  `issuerId` varchar(36) NOT NULL,
   `requestedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `approvedOn` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `quantity` int(11) NOT NULL,
   `isApproved` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`requestId`),
+  PRIMARY KEY (`id`),
   KEY `fk_issuer` (`issuerId`),
   KEY `fk_requester` (`requesterId`),
-  CONSTRAINT `fk_issuer` FOREIGN KEY (`issuerId`) REFERENCES `users` (`empId`),
-  CONSTRAINT `fk_requester` FOREIGN KEY (`requesterId`) REFERENCES `users` (`empId`)
+  CONSTRAINT `fk_issuer` FOREIGN KEY (`issuerId`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_requester` FOREIGN KEY (`requesterId`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -65,14 +65,15 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
+  `id` varchar(36) NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `dob` date NOT NULL,
   `role` varchar(10) NOT NULL,
   `password` varchar(50) NOT NULL,
   `empId` varchar(10) NOT NULL,
-  `isActive` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`empId`),
+  `isActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -94,12 +95,12 @@ DROP TABLE IF EXISTS `wallet`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wallet` (
-  `id` varchar(32) NOT NULL,
-  `empId` varchar(10) NOT NULL,
+  `id` varchar(36) NOT NULL,
+  `userId` varchar(36) NOT NULL,
   `amount` varchar(32) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `fk_users_wallet` (`empId`),
-  CONSTRAINT `fk_users_wallet` FOREIGN KEY (`empId`) REFERENCES `users` (`empId`)
+  KEY `fk_users_wallet` (`userId`),
+  CONSTRAINT `fk_users_wallet` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -121,4 +122,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-09 18:40:40
+-- Dump completed on 2019-05-10 15:10:15
